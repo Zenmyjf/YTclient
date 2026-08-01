46622cf4f11431a6f421688b693fa1cb39d29211
Zenmyjf <114481747+Zenmyjf@users.noreply.github.com>
2026-08-01 14:24:31 +0530

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        )
     }
 
     packaging {
